afd5f58e2ff9359e25950a3fdcb30209ad7a600a
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-08-27 17:25:46 -0400

    FormRegistroUsuario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AuthRoutingModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ReactiveFormsModule,
     RouterModule
   ]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidatorsService } from '../services/validators.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  formSubmited = false;
+  miFormulario = this.fb.group(
+    {
+      nombre: ['', [Validators.required]],
+      email: [
+        '',
+        [
+          Validators.pattern(this.validatorService.emailPattern),
+          Validators.required,
+        ],
+      ],
+      password: ['', [Validators.required]],
+      password2: ['', [Validators.required]],
+      terminos: [false, [Validators.requiredTrue]],
+    },
+    { validators: [this.validatorService.equalPasswords('password','password2')] }
+  );
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+    public validatorService: ValidatorsService
+  ) {}
 
-  ngOnInit(): void {
+  crearUsuario() {
+    this.formSubmited = true;
+    console.log(this.miFormulario);
+
+    if (this.miFormulario.valid) {
+      console.log(this.miFormulario.get('password2'))
+      console.log('Posteando formulario');
+    }
   }
 
+  campoValido(campo: string): boolean {
+    const control = this.miFormulario.get(campo)!;
+    if (control.invalid && this.formSubmited) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
